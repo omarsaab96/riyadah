3da71a725191e87ff3ffc7ad82d22c0158f50f23
omarsaab96 <omar.saab.gwy.iq>
2025-06-14 23:05:58 +0300

    .

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,14 +4,14 @@ const User = require('../models/User');
 
 // Create user
 router.post('/', async (req, res) => {
-  // try {
+  try {
   //   console.log('Incoming body:', req.body);
   //   const newUser = await User.create(req.body);
-  //   res.json(newUser);
-  // } catch (err) {
-  //   res.status(400).json({ error: err.message });
-  // }
-  res.status(200).json({ status: 'success' });
+    res.json(newUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+  // res.status(200).json({ status: 'success' });
 });
 
 // Get all users
