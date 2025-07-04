2886778115141f39ef946f9a80aca25ebf4d1635
omarsaab96 <omar.saab.gwy.iq>
2025-07-04 03:52:48 +0300

    .

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const Inventory = require('../models/Inventory');
 
 // Middleware to verify token
