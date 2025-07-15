5b7adc6e2be6a49b2ae3f550fd8444d36ecea051
omarsaab96 <omar.saab.gwy.iq>
2025-07-15 10:42:46 +0300

    .

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const Team = require('../models/Team');
+const mongoose = require('mongoose');
 
 const jwt = require("jsonwebtoken");
 
