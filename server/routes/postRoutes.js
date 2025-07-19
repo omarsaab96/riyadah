42e8b09f7413826e9f561a0a8fd93a225783f8b6
omarsaab96 <omar.saab.gwy.iq>
2025-07-19 19:58:03 +0300

    .

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     const { page = 1, limit = 10 } = req.query;
     try {
         const posts = await Post.find()
-            .populate('created_by', 'name avatar')
+            .populate('created_by', 'name image')
             .sort({ date: -1 })
             .skip((page - 1) * limit)
             .limit(parseInt(limit));
