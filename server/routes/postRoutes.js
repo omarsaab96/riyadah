eebbf9899adba9d103268f2bca206f9847ef5a8c
omarsaab96 <omar.saab.gwy.iq>
2025-07-22 21:52:26 +0300

    .

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -162,6 +162,8 @@ router.put('/delete/:id', authenticateToken, async (req, res) => {
         const post = await Post.findById(postId).populate('created_by', '_id name image');
         if (!post) return res.status(404).json({ message: 'Post not found' });
 
+        console.log('Post Creator:', post.created_by);
+
         // Only allow the creator to unlink
         if (post.created_by._id !== userId) {
             return res.status(403).json({ message: 'You are not authorized to unlink this post' });
