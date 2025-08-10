a69886a1832c379c2baa0712c94c957f86e61dd1
omarsaab96 <omar.saab.gwy.iq>
2025-08-10 06:24:03 +0300

    .

--- server/utils/notificationService.js
+++ server/utils/notificationService.js
@@ -2,6 +2,7 @@ const { Expo } = require('expo-server-sdk');
 const expo = new Expo();
 
 async function sendNotification(user, title, body, data = {}) {
+  console.log("received user=", user)
   if (!user.expoPushToken || !Expo.isExpoPushToken(user.expoPushToken)) {
     throw new Error('Invalid or missing Expo push token');
   }
