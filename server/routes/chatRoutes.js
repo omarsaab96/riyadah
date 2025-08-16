6d8381990650706bab5199eb22b22857d65d35dd
omarsaab96 <omar.saab.gwy.iq>
2025-08-16 20:41:54 +0300

    .

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -227,17 +227,6 @@ router.post("/:chatId/message", authenticateToken, async (req, res) => {
                     console.log('sending message "' + message.text + '" to ' + participant._id)
                     io.to(chatId).emit("newMessage", { chatId, message });
                 } else {
-                    //update message screen
-                    notifyChatListUpdate(participant._id, {
-                        _id: chat._id,
-                        participants: chat.participants,
-                        lastMessage: {
-                            text,
-                            senderId: userId,
-                            timestamp: message.timestamp,
-                        },
-                    });
-
                     //send notification
                     const userToNotify = await User.findOne({
                         _id: participant._id,
@@ -255,9 +244,18 @@ router.post("/:chatId/message", authenticateToken, async (req, res) => {
                         console.error(`Failed to send notification to user ${userToNotify._id}:`, err.message);
                     }
                 }
-
-
             }
+            
+            //update message screen
+            notifyChatListUpdate(participant._id, {
+                _id: chat._id,
+                participants: chat.participants,
+                lastMessage: {
+                    text,
+                    senderId: userId,
+                    timestamp: message.timestamp,
+                },
+            });
         }
 
         res.json({ success: true, message: "Message sent", data: message });
