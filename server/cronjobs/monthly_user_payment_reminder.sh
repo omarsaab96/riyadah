4d6d35bc4f367c1a69b81fe0a17c5c8202963f62
omarsaab96 <omar.saab.96@gmail.com>
2025-10-23 13:13:39 +0300

    .

--- server/cronjobs/monthly_user_payment_reminder.sh
+++ server/cronjobs/monthly_user_payment_reminder.sh
@@ -2,7 +2,7 @@
 echo "RUN - Cron#1 [$(date)] - Sending monthly user payment reminder" >> /var/www/riyadah/cronjobs/logs/monthly_user_payment_reminder.log
 
 # Example: call your Node.js script or API
-cd /var/www/riyadah/cronjobs
-/usr/bin/node monthlyUserPaymentReminder.js --monthly
+cd /var/www/riyadah/server/cronjobs
+/usr/bin/node monthlyUserPaymentReminder.js
 
 echo "DONE - Cron#1 [$(date)]" >> /var/www/riyadah/cronjobs/logs/monthly_user_payment_reminder.log
