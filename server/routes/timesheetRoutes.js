7de92d902aa0aa4c4251d42a3e0c50c845ff86ad
omarsaab96 <omar.saab.96@gmail.com>
2025-11-06 12:48:07 +0300

    .

--- server/routes/timesheetRoutes.js
+++ server/routes/timesheetRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Timesheet = require("../models/Timesheet");
-const auth = require("../middleware/auth");
 
 const authorizeRole = (role) => (req, res, next) => {
     if (!req.user || req.user.role !== role) {
