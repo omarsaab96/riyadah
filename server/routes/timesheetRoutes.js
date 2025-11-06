11da9fdd83d7b6652fa96536f1f06b084d52998f
omarsaab96 <omar.saab.96@gmail.com>
2025-11-06 12:53:46 +0300

    .

--- server/routes/timesheetRoutes.js
+++ server/routes/timesheetRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Timesheet = require("../models/Timesheet");
+const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
     const authHeader = req.headers['authorization'];
